1f81de30645162c876a2dca772f872053ee00b71
Shraddha Phadnis <phadnis.s@husky.neu.edu>
2017-11-18 16:28:08 -0500

    proxy code to handle set request

--- proxy.go
+++ proxy.go
@@ -4,144 +4,99 @@ import (
 	"net/http"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
+	"encoding/json"
+	"hash/fnv"
+	//"container/list"
 )
 
+type KeyVal struct {
+	encoding string `json:"encoding"`
+	data string `json:"data"`
+}
+
+type setObject struct {
+	key KeyVal `json:"key"`
+	value KeyVal `json:"value"`
+}
+
+func hashFunc(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32() 
+}
+
+var ServerList [1000][1000] setObject
 func main() {
 	/*var (
 	port = flag.String("p", "8080", "specify port to use defaulting to 8080");
 	)*/
+	//length := len(flag.Args())
 	flag.Parse();
+
 	fmt.Printf("length of arguments = %d",len(flag.Args()));
 
-	args := flag.Args()
+	for i:=0; i< len(flag.Args());i++ {
+
+	}
+	// args := flag.Args()
 	//fmt.Printf("args[1] = %d", args[1]);
 	finish := make(chan bool)
 	
-	server1 := http.NewServeMux()
-	server1.HandleFunc("/GET", getHandlerServer1)
-	server1.HandleFunc("/post", postHandlerServer1)
-
-	server2 := http.NewServeMux()
-	server2.HandleFunc("/GET", getHandlerServer2)
-	server2.HandleFunc("/post", postHandlerServer2)
-	
-	server3 := http.NewServeMux()
-	server3.HandleFunc("/GET", getHandlerServer3)
-	server3.HandleFunc("/post", postHandlerServer3)
-	
-	server4 := http.NewServeMux()
-	server4.HandleFunc("/GET", getHandlerServer4)
-	server4.HandleFunc("/post", postHandlerServer4)
+	proxyserver := http.NewServeMux()
+	proxyserver.HandleFunc("/set", setHandleFunc)
+	proxyserver.HandleFunc("/fetch", fetchHandleFunc)
+	proxyserver.HandleFunc("/query", queryHandleFunc)
 
 	go func() {
-		http.ListenAndServe(":"+args[0], server1)
+		http.ListenAndServe(":8080", proxyserver)
 	
 	}()
-
-	go func() {
-		http.ListenAndServe(":"+args[1], server2)
-	}()
-
-	go func() {
-		http.ListenAndServe(":"+args[2], server3)
-	}()
 	
-	go func() {
-		http.ListenAndServe(":"+args[3], server4)
-	}()
-	
-
-
-
 	<-finish
 }
 
 var results[] string
 
 /* get handlers definitions*/
-func getHandlerServer1(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Listening on server1 port:"))
-}
-
-func getHandlerServer2(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Listening on server2 port:"))
-}
-
-func getHandlerServer3(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Listening on server3:"))
-}
-
-func getHandlerServer4(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Listening on server4:"))
+func setHandleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Inside setHandleFunc:"))
+	switch r.Method {
+		case "GET":
+			http.Error(w, "GET not supported", 400)
+		case "POST":
+			http.Error(w, "POST not supported", 400)
+    	case "PUT":
+    		// allocation using make
+    		request := make([]setObject,0)
+    		decode := json.NewDecoder(r.Body)
+    		err := decode.Decode(&request)
+    		if err != nil {
+    			http.Error(w,err.Error(),400)
+    		}
+    	
+    		for i:=0; i<len(request); i++ {
+    			//fmt.Printf("keyvalue = %d",request[i].key.data)
+    			server := hashFunc(request[i].key.data) % (uint32)(len(flag.Args()))
+    			ServerList[server][i] = request[i]
+    		}
+    		//TBD
+    	case "DELETE":
+    		http.Error(w, "DELETE not supported", 400)
+    	default: 
+    		http.Error(w, "unknown request", 400)		
+		}
+		w.Write([]byte("Inside setHandleFunc ***:"))
 }
 
 /* post handlers */
 
-func postHandlerServer1(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Listening on server1: POST "))
-
-	if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body",
-				http.StatusInternalServerError)
-		}
-		results = append(results,string(body))
-
-		fmt.Fprint(w, "POST done")
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
+func fetchHandleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("fetchHandleFunc: fetch"))
 }
 
-func postHandlerServer2(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Listening on server2 port: POST "))
+/* post handlers */
 
-	if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body",
-				http.StatusInternalServerError)
-		}
-		results = append(results,string(body))
-
-		fmt.Fprint(w, "POST done")
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
-}
-
-func postHandlerServer3(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Listening on server3 port: POST "))
-
-	if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body",
-				http.StatusInternalServerError)
-		}
-		results = append(results,string(body))
-
-		fmt.Fprint(w, "POST done")
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
-}
-
-func postHandlerServer4(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Listening on server4 port"))
-
-	if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body",
-				http.StatusInternalServerError)
-		}
-		results = append(results,string(body))
-
-		fmt.Fprint(w, "POST done")
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
+func queryHandleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("queryHandleFunc: query"))
 }
